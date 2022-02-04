30c698465f4a73edcdb69ea43d7d66bdfc8b04bd
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2022-02-04 18:36:20 +0600

    Improved preprocessing

--- 00-data-preprocessing/preprocess.py
+++ 00-data-preprocessing/preprocess.py
@@ -1,36 +1,56 @@
 import os
 from bnlp import NLTKTokenizer
-from bnlp.corpus import letters, digits
+from bnlp.corpus import letters, digits, punctuations, vower_mark
 
 def pure_sentence(str):
     """
-    Check if a str is a pure bengali sentence containing
+    Check if str is a pure bengali sentence containing
     only bengali letters and digits
     """
-    valids = letters + digits
-    return all(thing in set(sentence_token) for thing in valids)
+    valids = letters + digits + vower_mark + punctuations + "্"
+    return len(str) > 2 and all(thing in valids for thing in set(str))
 
 
-bnltk = NLTKTokenizer()
+def process_sentence(str):
+    """
+    Remove punctuations from a sentence
+    """
+    for symbol in punctuations:
+        str = str.replace(symbol, ' ')
+    str = " ".join(str.split())
+    return str
 
-raw_data_filepath = "../data/bn_dedup.txt"
-processed_data_dir = "../processed_data/"
-processed_data_filepath = processed_data_dir + "processed_data.txt"
 
-os.makedirs(processed_data_dir, exist_ok=True)
-if os.path.exists(processed_data_filepath):
-    os.remove(processed_data_filepath)
-processed_data_file = open(processed_data_filepath, "a+")
+def preprocess_file(raw_data_filepath, processed_data_dir, processed_filename):
+    """
+    Reads from raw_data_filepath, and writes processed sentences from
+    the file into processed_data_dir/processed_filename one sentence per line.
+    """
+    bnltk = NLTKTokenizer()
+    os.makedirs(processed_data_dir, exist_ok=True)
+    if os.path.exists(processed_data_filepath):
+        os.remove(processed_data_filepath)
+    processed_data_file = open(processed_data_filepath, "a+")
 
-print(f"Processing {raw_data_filepath}")
-with open(raw_data_filepath) as raw_data_file:
-    for i, line in enumerate(raw_data_file):
-        sentence_tokens = bnltk.sentence_tokenize(line)
-        for sentence_token in sentence_tokens:
-            if pure_sentence(sentence_token):
-                processed_data_file.write(sentence_token)
-                processed_data_file.write("\n")
-        if i % 1000 == 0:
-            print(f"Processed {i} lines")
+    print(f"Processing {raw_data_filepath}")
+    with open(raw_data_filepath) as raw_data_file:
+        for i, line in enumerate(raw_data_file):
+            sentence_tokens = bnltk.sentence_tokenize(line)
+            for sentence_token in sentence_tokens:
+                if pure_sentence(sentence_token):
+                    processed_sentence = process_sentence(sentence_token)
+                    processed_data_file.write(processed_sentence)
+                    processed_data_file.write("\n")
+            if i % 1000 == 0:
+                print(f"Processed {i} lines")
 
-processed_data_file.close()
+    processed_data_file.close()
+
+
+if __name__ == '__main__':
+    raw_data_filepath = "../data/bn_dedup.txt"
+    processed_data_dir = "../processed_data/"
+    processed_data_filepath = processed_data_dir + "processed_data.txt"
+    preprocess_file(raw_data_filepath,
+                    processed_data_dir,
+                    processed_data_filepath)
