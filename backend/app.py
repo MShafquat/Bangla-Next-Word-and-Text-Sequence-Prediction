0767b019177b8e689ffc66b51ad29d64a7eb01c7
Ahad Sheikh MLab <ahadsheikh1999@gmail.com>
2022-03-17 16:05:00 +0600

    Backend Initilize

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask,request,jsonify
+import numpy as np
+import pickle
+
+model = pickle.load(open('model.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
