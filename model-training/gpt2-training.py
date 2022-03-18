775d15cc6704a76ad20c8f206d144a378e520e9f
Shafquat Mahbub <mahbub.shafquat@gmail.com>
2022-03-18 17:49:36 +0600

    Changed gpt code

--- model-training/gpt2-training.py
+++ model-training/gpt2-training.py
@@ -1,37 +1,56 @@
 from pathlib import Path
+import pickle
 from datasets import load_dataset
-from transformers import AutoTokenizer, TFGPT2LMHeadModel
-from transformers import TextDataset, DataCollatorForLanguageModeling
+from transformers import AutoTokenizer, DataCollatorForLanguageModeling, GPT2LMHeadModel
 from transformers import Trainer, TrainingArguments
-import tensorflow as tf
 
 # get data directory
 project_root = Path(__file__).resolve().parents[1]
 data_dir = project_root / 'processed_data/'
-files = [str(file) for file in Path(data_dir).glob('**/*.txt')][:1]
+files = [str(file) for file in Path(data_dir).glob('**/*.txt')]
 
 tokenizer = AutoTokenizer.from_pretrained("flax-community/gpt2-bengali")
+tokenizer.add_special_tokens({'pad_token': '<pad>', 'bos_token': '<s>', 'eos_token': '</s>'})
+model = GPT2LMHeadModel.from_pretrained("flax-community/gpt2-bengali")
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
-with tf.distribute.get_strategy().scope():
-    model = TFGPT2LMHeadModel.from_pretrained(
-        "flax-community/gpt2-bengali", from_pt=True)
-    model.resize_token_embeddings(len(tokenizer))
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
-    model.compile(optimizer=optimizer, loss=model.compute_loss)
+dataset = load_dataset('text', data_files={'train': files[:1], 'eval': files[1:2]})
+dataset = dataset.map(lambda example: tokenizer(example['text']), batched=True, num_proc=4, remove_columns=['text'])
 
-train_dataset = load_dataset('text', data_files=files)
-train_dataset = train_dataset.map(lambda examples: tokenizer(examples['text']), batched=True, num_proc=4, remove_columns=['text'])
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer, mlm=False, return_tensors="tf"
+training_arguments = TrainingArguments(
+    output_dir=str(project_root / 'models/bn-gpt2'),
+    overwrite_output_dir=True,
+    num_train_epochs=3,
+    save_total_limit=2,
+    save_steps=1000,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    gradient_accumulation_steps=32,
+    learning_rate=1e-5,
+    fp16=True,
+    fp16_opt_level='O1',
+    max_grad_norm=1.0,
+    resume_from_checkpoint=str(project_root / 'models/bn-gpt2'),
+    logging_dir=str(project_root / 'logs/bn-gpt2'),
+    logging_first_step=False,
+    logging_steps=500,
+    # do_eval=True,
+    seed=42,
 )
 
-train_dataset = train_dataset.to_tf_dataset(
-    batch_size=4, collate_fn=data_collator
+trainer = Trainer(
+    model=model,
+    args=training_arguments,
+    tokenizer=tokenizer,
+    train_dataset=dataset['train'],
+    # eval_dataset=dataset['eval'],
 )
-earlystop = tf.keras.callbacks.EarlyStopping(
-    monitor='val_loss', min_delta=0, patience=5, mode='auto')
-checkpoint = tf.keras.callbacks.ModelCheckpoint(str(
-    project_root / 'models/bn_gpt2/bn_gpt2_{epoch:02d}_{val_loss:.4f}.h5'),
-    monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-history = model.fit(train_dataset, epochs=100, batch_size=128,
-                    callbacks=[checkpoint])
+
+trainer.train()
+trainer.save_model()
+
+history = trainer.logs()
+print(history)
+
+with open(str(project_root / 'models/bn_gpt2/history'), 'wb') as file_pi:
+    pickle.dump(history.history, file_pi)
