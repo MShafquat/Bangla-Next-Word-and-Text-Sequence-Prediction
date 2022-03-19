f4b9734adafd33e5e7b84202f8545b974de54e5f
Ahad Sheikh MLab <ahadsheikh1999@gmail.com>
2022-03-19 15:21:36 +0600

    take command line input

--- data-collection/ebanglalibrary_scrape.py
+++ data-collection/ebanglalibrary_scrape.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 import requests as req
-import time
 import sys
 import os
 from pathlib import Path
+from tqdm import tqdm
 
 
 def program_close():
@@ -19,6 +19,7 @@ def save_state(id):
     with open(f'{data_dir}/track.txt', 'w+') as f:
         f.write(str(id))
 
+
 def scrape(url: str) -> str:
     src = req.get(url)
     soup = BeautifulSoup(src.content, 'html.parser')
@@ -31,77 +32,64 @@ def scrape(url: str) -> str:
             header = soup.find('span', class_='post post-post current-item').text.strip()
             ptags = soup.find('div', class_='entry-content entry-content-single').findAll('p')
             textp = [p.text for p in ptags]
-            text = ' '.join(textp) 
+            text = ' '.join(textp)
             return author, book, header, text
         except AttributeError as ae:
             return ''
     else:
         return ''
 
-def runlinks(base='https://www.ebanglalibrary.com/', begin=1, maxid=104453):
-    id = begin
 
+def runlinks(base='https://www.ebanglalibrary.com/', begin=1, maxid=104453):
     data_dir = Path(__file__).resolve().parent / 'data'
 
     try:
-        d_sum = 0
-        c = 1
+        pbar = tqdm(range(1, maxid + 1))
+        for id in pbar:
+            pbar.set_description(f"Scrapping Chapter: {id}")
 
-        while True:
-            start = time.time()
-            print('Running for link', base+str(id), '...', end='')
-            l = scrape(base + str(id))
-            if(len(l) == 0):
-                id += 1
-                c += 1
+            if id < begin:
                 continue
-            author = l[0]; book = l[1]; header = l[2]; text = l[3]
-            
+
+            l = scrape(base + str(id))
+
+            if (len(l) == 0):
+                continue
+
+            author = l[0];
+            book = l[1];
+            header = l[2];
+            text = l[3]
+
+            author = author.replace('/', '_')
+            book = book.replace('/', '_')
+
             author_path = f"{data_dir}/{author}"
             if not os.path.exists(author_path):
                 os.makedirs(author_path)
-            
+
             with open(f"{author_path}/{book}.txt", 'a') as f:
                 f.write(header + '\n' + text)
-            
-            id += 1
-
-            end = time.time()
-
-            # Estimated Time Calculation
-            duration = end-start
-            d_sum += duration
-            avg_d = d_sum / c
-            total_time = avg_d*(maxid-begin+1)
-            elapsed_t = c*avg_d
-            time_needed = total_time-elapsed_t
-            hours = int(time_needed / 3600)
-            extra = time_needed % 3600
-            minutes = int(extra / 60)
-            seconds = int(extra % 60)
-            
-            print(f"Estimated time needed: {hours}:{minutes}:{seconds}.", end='\r')
-            c += 1
 
     except KeyboardInterrupt as ke:
         save_state(id)
         program_close()
-    
+
     except Exception as e:
         print(e)
         save_state(id)
         program_close()
 
 
-def runscrape():
+def runscrape(begin, maxid):
     data_dir = Path(__file__).resolve().parent / 'data'
     try:
         with open(f'{data_dir}/track.txt') as f:
             r = f.read()
             if len(r) != 0:
                 try:
-                    runlinks(begin=int(r))
+                    runlinks(begin=int(r), maxid=maxid)
                 except ValueError:
-                    runlinks(begin=1)
+                    runlinks(begin=1, maxid=maxid)
     except FileNotFoundError as e:
-        runlinks(begin=1)
+        runlinks(begin=begin, maxid=maxid)
--- data-collection/main.py
+++ data-collection/main.py
@@ -1,5 +1,12 @@
 # Scrapping EBanglaLibrary
 from ebanglalibrary_scrape import runscrape
+import sys
 
 if __name__ == '__main__':
-    runscrape()
+    if len(sys.argv) == 3:
+        try:
+            begin = int(sys.argv[1])
+            maxid = int(sys.argv[2])
+            runscrape(begin=begin, maxid=maxid)
+        except Exception as e:
+            print(e)
