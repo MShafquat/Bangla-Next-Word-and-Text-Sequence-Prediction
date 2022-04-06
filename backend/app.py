f97861399016d1ee7b3be8aba4069c67f84c610c
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2022-04-06 22:26:58 +0600

    gpt2 model

--- backend/app.py
+++ backend/app.py
@@ -4,20 +4,13 @@ from flask import send_file
 
 app = Flask(__name__)
 
-@app.route('/model')
-def model():
-    path = "mlmodels/bn_lstm.h5"
-    return send_file(path, as_attachment=True)
-
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict/lstm', methods=['GET'])
 def predict():
-    if request.method == 'GET':
-        return 'GET'
-    elif request.method == 'POST':
-        data = request.json
-        return jsonify(data)
-    else:
-        return 'UNKNOWN'
+    return 'GET'
+
+@app.route('/predict/gpt-2', methods=['GET'])
+def predict():
+    return jsonify(['ami', 'jani', 'ki', 'hdf'])
     
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
